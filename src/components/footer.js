811a429d2ad2f797e26f4fbe4f695a4498d8b3e0
varunt23 <varuntanna23@gmail.com>
2020-05-17 02:34:56 -0400

    fixed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
     `)
     return (
         <footer className = {footerStyles.footer}>
-            <p> Creater by {data.site.siteMetadata.author}, 2020 </p>
+            <p className = {footerStyles.footnote}>  Created by {data.site.siteMetadata.author}, © 2020 </p>
         </footer>
 
     )
